6598002c05daadc7c9ffa6017940b4813eeb86cd
Deepak Bhari <deepak.bhari1@Deepaks-MBP.default>
2019-05-01 15:57:28 +0100

    REASONS WHY WILL NOT WORK

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,8 +1,18 @@
 import jsonPlaceholder from "../apis/jsonPlaceholder";
 
 export const fetchPosts = async () => {
-    
   //bad approach!!!
+  //Will result in error - "actions must be plain objects / use custom middleware for async actions..."
+
+  /* 
+  Reasons:
+
+  1. Action creators must return plain JS objects with a type property - we are not! Rememnber this gets
+  transpiled down to ES5 and the code thats generated does not return a plain JS object - check in babel.io
+
+  2. By the time our action gets to a reducer, we won't have fetched our data!
+  */
+
   const response = await jsonPlaceholder.get("/posts");
 
   return {
