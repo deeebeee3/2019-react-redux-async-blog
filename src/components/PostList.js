ce680bc72b6d258e571227b29422a4d90fba6a8e
Deepak Bhari <deepak.bhari1@Deepaks-MBP.default>
2019-05-01 14:28:49 +0100

    as soon as component renders on screen, we will be calling action creator, will return an action with type FETCH_POSTS

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,9 +1,22 @@
 import React from "react";
+import { connect } from "react-redux";
+
+import { fetchPosts } from "../actions";
 
 class PostList extends React.Component {
+  componentDidMount() {
+    this.props.fetchPosts();
+  }
+
   render() {
     return <div>Post List</div>;
   }
 }
 
-export default PostList;
+//Passing in null to connect as first arg as we do not have a mapStateToProps function right now
+//At present there is no state / data to get into this component
+//Pass in action creator (fetchPosts) as second argument
+export default connect(
+  null,
+  { fetchPosts: fetchPosts }
+)(PostList);
