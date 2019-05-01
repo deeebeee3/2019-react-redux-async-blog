12dfdd0b9913e58c866932882458559e956c0a6a
Deepak Bhari <deepak.bhari1@Deepaks-MBP.default>
2019-05-01 15:35:39 +0100

    WRONG WAY - looks like correct way to return action object with response on payload property...but it is breaking the rules of redux

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,12 @@
 import jsonPlaceholder from "../apis/jsonPlaceholder";
 
-export const fetchPosts = () => {
+export const fetchPosts = async () => {
+    
+  //bad approach!!!
+  const response = await jsonPlaceholder.get("/posts");
+
   return {
-    type: "FETCH_POSTS"
+    type: "FETCH_POSTS",
+    payload: response
   };
 };
