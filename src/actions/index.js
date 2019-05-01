ddc0dec6703ea8eceb5684115cb44704298327cd
Deepak Bhari <deepak.bhari1@Deepaks-MBP.default>
2019-05-01 19:24:16 +0100

    CORRECT WAY to get data from an api

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,22 +1,15 @@
 import jsonPlaceholder from "../apis/jsonPlaceholder";
 
-export const fetchPosts = async () => {
-  //bad approach!!!
-  //Will result in error - "actions must be plain objects / use custom middleware for async actions..."
+export const fetchPosts = () => {
 
-  /* 
-  Reasons:
+  //action creator will now retun an async function instead of a plain javascript object
+  //our middleware (thunk) will be able to deal with this - dispatch will be executed once
+  //data recieved from api
 
-  1. Action creators must return plain JS objects with a type property - we are not! Rememnber this gets
-  transpiled down to ES5 and the code thats generated does not return a plain JS object - check in babel.io
+  return async function(dispatch, getState) {
+    const response = await jsonPlaceholder.get("/posts");
 
-  2. By the time our action gets to a reducer, we won't have fetched our data!
-  */
+    dispatch({ type: "FETCH_POSTS", payload: response })
+  }
 
-  const response = await jsonPlaceholder.get("/posts");
-
-  return {
-    type: "FETCH_POSTS",
-    payload: response
-  };
 };
