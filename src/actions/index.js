fd747e01653be1b54726810fa08496e666d04b88
Deepak Bhari <deepak.bhari1@Deepaks-MBP.default>
2019-05-01 14:37:13 +0100

    create axios config file with base url for typicode API, import into actions index.js file

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,7 @@
+import jsonPlaceholder from "../apis/jsonPlaceholder";
+
 export const fetchPosts = () => {
-    return {
-        type: 'FETCH_POSTS'
-    }
+  return {
+    type: "FETCH_POSTS"
+  };
 };
