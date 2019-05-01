373f89ca20f58af8f3fbb055ccbd8d014c489c63
Deepak Bhari <deepak.bhari1@Deepaks-MBP.default>
2019-05-01 19:31:26 +0100

    small refactor of action creator to streamline it - it is the equivalent of the previous action creator

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,15 +1,9 @@
 import jsonPlaceholder from "../apis/jsonPlaceholder";
 
-export const fetchPosts = () => {
-
-  //action creator will now retun an async function instead of a plain javascript object
-  //our middleware (thunk) will be able to deal with this - dispatch will be executed once
-  //data recieved from api
-
-  return async function(dispatch, getState) {
+export const fetchPosts = () => async dispatch => {
     const response = await jsonPlaceholder.get("/posts");
 
     dispatch({ type: "FETCH_POSTS", payload: response })
   }
 
-};
+
