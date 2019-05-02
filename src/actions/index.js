19fb770612528af19539bc3bbc59639596c21d4b
Deepak Bhari <deepak.bhari1@Deepaks-MBP.default>
2019-05-02 23:09:11 +0100

    Alternate overfetching solution - see a brief little pause as fetching our list of posts, then see console log of all the posts we have fetched

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,10 +1,10 @@
 import _ from 'lodash';
 import jsonPlaceholder from "../apis/jsonPlaceholder";
 
-export const fetchPostsAndUsers = () => async dispatch => {
-  console.log('about to fetch posts');
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
   await dispatch(fetchPosts());
-  console.log('fetched posts!');
+  
+  console.log(getState().posts);
 }
 
 export const fetchPosts = () => async dispatch => {
