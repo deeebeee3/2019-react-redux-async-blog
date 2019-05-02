8b0ab81670bb245fdfcc2d7500e8a6f892069f6d
Deepak Bhari <deepak.bhari1@Deepaks-MBP.default>
2019-05-02 22:39:20 +0100

    One time memoization issues

--- src/actions/index.js
+++ src/actions/index.js
@@ -7,15 +7,11 @@ export const fetchPosts = () => async dispatch => {
   dispatch({ type: "FETCH_POSTS", payload: response.data });
 };
 
-// export const fetchUser = id => async dispatch => {
-//   const response = await jsonPlaceholder.get(`/users/${id}`);
 
-//   dispatch({ type: "FETCH_USER", payload: response.data });
-// };
-
+//ISSUE WITH THIS - can call this action creator only one time with each unique user id
+//So won't be able to refetch a user with this action creator if ever needed to in the future...
+//IN REAL LIFE - highly likely will need to refetch different resources over time...
 export const fetchUser = (id) => (dispatch) => _fetchUser(id, dispatch);
-
-
 const _fetchUser = _.memoize( async (id, dispatch) => {
   const response = await jsonPlaceholder.get(`/users/${id}`);
 
