0ef05e281ccb7135f9fd1fbb33cbdb3df8a87666
Deepak Bhari <deepak.bhari1@Deepaks-MBP.default>
2019-05-02 22:23:21 +0100

    memoization - refactor our async action creator to use arrow functions again

--- src/actions/index.js
+++ src/actions/index.js
@@ -13,11 +13,9 @@ export const fetchPosts = () => async dispatch => {
 //   dispatch({ type: "FETCH_USER", payload: response.data });
 // };
 
-export const fetchUser = function(id) {
-  return function(dispatch){
+export const fetchUser = (id) =>
+   (dispatch) =>
     _fetchUser(id, dispatch);
-  };
-};
 
 
 const _fetchUser = _.memoize( async (id, dispatch) => {
