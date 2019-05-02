78bd2be3bdbcf0fc47f10acfbc2bf3e083905cbe
Deepak Bhari <deepak.bhari1@Deepaks-MBP.default>
2019-05-02 23:35:07 +0100

    Alternate overfetching solution - don't want the UserHeader component to fetch its own data any more

--- src/components/UserHeader.js
+++ src/components/UserHeader.js
@@ -1,13 +1,7 @@
 import React from "react";
 import { connect } from "react-redux";
 
-import { fetchUser } from "../actions";
-
 class UserHeader extends React.Component {
-  componentDidMount() {
-    this.props.fetchUser(this.props.userId);
-  }
-
   render() {
     //console.log(this.props.user);
 
@@ -26,6 +20,5 @@ const mapStateToProps = (state, ownProps) => {
 };
 
 export default connect(
-  mapStateToProps,
-  { fetchUser }
+  mapStateToProps
 )(UserHeader);
